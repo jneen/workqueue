c9cc1afcc7ef42a5c5b142e509d23a06737f49dd
Jay Adkisson <jjmadkisson@gmail.com>
2011-11-07 17:38:09 -0800

    spec manually calling #run

--- spec/workqueue_spec.rb
+++ spec/workqueue_spec.rb
@@ -1,5 +1,7 @@
 describe WorkQueue do
-  it 'preserves order' do
+  let(:mutex) { Mutex.new }
+
+  it 'preserves order of results' do
     queue = WorkQueue.new([1,2,3]) do |x|
       # the sleep ensures they actually run in opposite order
       sleep 0.5 - 0.1*x
@@ -7,5 +9,28 @@ describe WorkQueue do
     end.run
 
     assert { queue.results == [2, 3, 4] }
+  end
+
+  it %[doesn't do anything unless #run is called] do
+    aggregator = []
+
+    queue = WorkQueue.new([1,2,3]) do |x|
+      mutex.synchronize { aggregator << x + 1 }
+    end
+
+    aggregator << 0
+
+    assert { queue.results == [] }
+
+    queue.run
+
+    # aggregator is undefined here, since we're
+    # running parallel to the workers...
+
+    # so let's join them!
+    # (calling #results also does this)
+    queue.join
+
+    assert { aggregator == [0, 2, 3, 4] }
   end
 end
