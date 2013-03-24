ba43f878e38b5b883f13488e462101872c2870fe
http://jayferd.us/ <jjmadkisson@gmail.com>
2013-03-23 22:17:30 -0700

    use less ambiguous syntax

--- spec/workqueue_spec.rb
+++ spec/workqueue_spec.rb
@@ -7,7 +7,7 @@ describe WorkQueue do
   it 'preserves order of results' do
     queue = WorkQueue.new([1,2,3]) do |x|
       # the sleep ensures they actually run in opposite order
-      sleep 0.5 - 0.1*x
+      sleep(0.5 - 0.1*x)
       x + 1
     end.run
 
